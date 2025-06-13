954265865d8645f1728478f212dac667636b1aea
Marcus Aurélio do Bomfim Visgueira <visgueira@gmail.com>
2025-06-13 11:58:05 -0300

    Update gamma_wlad6.py

--- gamma_wlad6.py
+++ gamma_wlad6.py
@@ -23,18 +23,12 @@ def check_login():
     st.sidebar.subheader("🔐 Acesso Restrito")
     username = st.sidebar.text_input("Usuário")
     password = st.sidebar.text_input("Senha", type="password")
-    pass
 
-    # Usuários válidos
+    # Usuários válidos (usando secrets)
     usuarios = st.secrets["auth"]["usuarios"]
     valid_pass = st.secrets["auth"]["passw"]
-    
-    #'''valid_users = {
-   #     "admin": "Pqw74nbc8z@&",
-    #    "wlca": "17151" 
-   # }'''
 
-    if username in usuarios and password in valid_pass: #if username in valid_users and password == valid_users[username]:
+    if username in usuarios and password in valid_pass:
         return True
     elif username and password:
         st.sidebar.error("Usuário ou senha incorretos.")
@@ -50,11 +44,12 @@ def main():
     st.title("Análise de Gamma Exposure")
 
     with st.sidebar:
+        st.markdown("### 📁 Parâmetros de Entrada")
         uploaded_file = st.file_uploader("Envie o arquivo quotedata.csv", type="csv")
         bar_width1 = st.number_input("Largura das barras do Gráfico 1 (ex: 0.3)", min_value=0.01, max_value=1.0, step=0.01, value=0.3)
         bar_width2 = st.number_input("Largura das barras do Gráfico 2 (ex: 0.2)", min_value=0.01, max_value=1.0, step=0.01, value=0.2)
         levels_input = st.slider("Quantidade de níveis (resolução do gráfico 3)", min_value=20, max_value=100, step=5, value=60)
-        bar_color1 = st.color_picker("Cor das barras do Gráfico 1",  '#1a76ff')
+        bar_color1 = st.color_picker("Cor das barras do Gráfico 1", '#1a76ff')
         bar_color2 = st.color_picker("Cor das barras do Gráfico 2 (Call)", '#00cc96')
         bar_color3 = st.color_picker("Cor das barras do Gráfico 2 (Put)", '#ef553b')
 
@@ -170,4 +165,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
