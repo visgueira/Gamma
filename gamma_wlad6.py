3f32827218b9e4172c3d787f963dd28047fe275f
Marcus Aurélio do Bomfim Visgueira <visgueira@gmail.com>
2025-06-15 17:00:28 -0300

    Update gamma_wlad6.py

--- gamma_wlad6.py
+++ gamma_wlad6.py
@@ -25,8 +25,8 @@ def main():
 
     with st.sidebar:
         uploaded_file = st.file_uploader("Envie o arquivo quotedata.csv", type="csv")
-        bar_width1 = st.number_input("Largura das barras do Gráfico 1 (ex: 0.3)", min_value=0.01, max_value=1.0, step=0.01, value=0.3)
-        bar_width2 = st.number_input("Largura das barras do Gráfico 2 (ex: 0.2)", min_value=0.01, max_value=1.0, step=0.01, value=0.2)
+        bar_width1 = st.number_input("Largura das barras do Gráfico 1 (ex: 0.3)", min_value=0.01, max_value=15.0, step=0.01, value=0.3)
+        bar_width2 = st.number_input("Largura das barras do Gráfico 2 (ex: 0.2)", min_value=0.01, max_value=15.0, step=0.01, value=0.2)
         levels_input = st.slider("Quantidade de níveis (resolução do gráfico 3)", min_value=20, max_value=100, step=5, value=60)
         bar_color1 = st.color_picker("Cor das barras do Gráfico 1",  '#1a76ff')
         bar_color2 = st.color_picker("Cor das barras do Gráfico 2 (Call)", '#00cc96')
